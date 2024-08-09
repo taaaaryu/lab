2486aeeeed46e8c895e8b886f7cabaf32eb78e39
taaaaryu <taruu6109@gmail.com>
2024-08-09 14:56:02 +0900

    commit

--- Opti/analitical_eval/check_upperx%_software.py
+++ Opti/analitical_eval/check_upperx%_software.py
@@ -10,7 +10,7 @@ import statistics
 # パラメータ
 n = 10  # サービス数
 H = n*2  # サーバリソース
-h_add = 1.0  # サービス数が1増えるごとに使うサーバ台数の増加
+h_adds = [0.5,1,1.5]  # サービス数が1増えるごとに使うサーバ台数の増加
 
 # 定数
 
@@ -50,64 +50,85 @@ def calc_sw_resource(comb):
 software_availabilities = []
 combination_lengths = []
 results = []
-
-
-for num_software in softwares:
-    all_combinations = generate_service_combinations(services, num_software)
-    all_redundancies = generate_redundancy_combinations(num_software, H, h_add)
-    progress_tqdm = tqdm(total=len(all_combinations) + len(all_redundancies), unit="count")
-    
-    redundancy_result = []
-
-    # 冗長化度合いによるCDFの計算
-    for redundancy in all_redundancies:
-        max_system_avail = -1
-        best_combination = None
-        for comb in all_combinations:
-            total_servers = sum(redundancy[i] * ((h_add * (len(comb[i]) - 1)) + 1) for i in range(len(comb)))
-            if total_servers <= H:
-                if alloc <= total_servers:
-                    software_availability = [calc_software_av(group, service_avail) * server_avail for group in comb]
-                    system_avail = np.prod([1 - (1 - sa) ** int(r) for sa, r in zip(software_availability, redundancy)])
-                    if system_avail > max_system_avail:
-                        max_system_avail = system_avail
-                        best_combination = comb
-        if best_combination:
-            results.append((redundancy, best_combination, max_system_avail))
-            combination_lengths.append((max_system_avail, len(best_combination)))
-        progress_tqdm.update(1)
-
-progress_tqdm.close()
-
-# 上位x%のソフトウェア数を計算
-top_x_count = int(top_x_percent*len(combination_lengths))
-
-# 上位x%のシステム可用性に対応するbest_combinationの長さの分布
-results.sort(key=lambda x: x[2], reverse=True)
-combination_lengths.sort(key=lambda x: x[0], reverse=True)
-std_sw_resource = []
-
-#print(results[:20])
-comb = [comb for red, comb, avail in results[:top_x_count]]
-each_sw_resource = [calc_sw_resource(k) for k in comb]
-for j in each_sw_resource:
-    if len(j)==1:
-        continue
-    std_sw_resource.append(statistics.stdev(j))
-print(std_sw_resource)
-top_combination_lengths = [length for avail, length in combination_lengths[:top_x_count]]
-#print(top_combination_lengths)
-
-count_software = [top_combination_lengths.count(i) for i in range(1,num_software+1)]
-
-# 標準偏差のCDFを計算し、プロット
-std_sw_resource.sort()
-cdf = np.arange(len(std_sw_resource)) / float(len(std_sw_resource) - 1)
-
 fig, ax = plt.subplots(figsize=(12, 8))
-ax.plot(std_sw_resource, cdf, marker='o', linestyle='-', color='b')
+i=0
+
+for h_add in h_adds:
+    for num_software in softwares:
+        all_combinations = generate_service_combinations(services, num_software)
+        all_redundancies = generate_redundancy_combinations(num_software, H, h_add)
+        progress_tqdm = tqdm(total=len(all_combinations) + len(all_redundancies), unit="count")
+        
+        redundancy_result = []
+
+        # 冗長化度合いによるCDFの計算
+        for redundancy in all_redundancies:
+            max_system_avail = -1
+            best_combination = None
+            for comb in all_combinations:
+                total_servers = sum(redundancy[i] * ((h_add * (len(comb[i]) - 1)) + 1) for i in range(len(comb)))
+                if total_servers <= H:
+                    if alloc <= total_servers:
+                        software_availability = [calc_software_av(group, service_avail) * server_avail for group in comb]
+                        system_avail = np.prod([1 - (1 - sa) ** int(r) for sa, r in zip(software_availability, redundancy)])
+                        if system_avail > max_system_avail:
+                            max_system_avail = system_avail
+                            best_combination = comb
+            if best_combination:
+                results.append((redundancy, best_combination, max_system_avail))
+                combination_lengths.append((max_system_avail, len(best_combination)))
+            progress_tqdm.update(1)
+
+    progress_tqdm.close()
+
+    # 上位x%のソフトウェア数を計算
+    top_x_count = int(top_x_percent*len(combination_lengths))
+
+    # 上位x%のシステム可用性に対応するbest_combinationの長さの分布
+    results.sort(key=lambda x: x[2], reverse=True)
+    combination_lengths.sort(key=lambda x: x[0], reverse=True)
+    std_sw_resource = []
+    std_sw_resource_all = []
+
+    #全ての組み合わせの標準偏差
+    comb_all = [comb for red, comb, avail in results]
+    each_sw_resource_all = [calc_sw_resource(k) for k in comb_all]
+    for j in each_sw_resource_all:
+        if len(j)==1:
+            continue
+        std_sw_resource_all.append(statistics.stdev(j))
+    
+    top_combination_lengths = [length for avail, length in combination_lengths[:top_x_count]]
+    
+    
+    #print(results[:20])
+    #TOP1%の組み合わせのSWリソースの標準偏差
+    comb = [comb for red, comb, avail in results[:top_x_count]]
+    each_sw_resource = [calc_sw_resource(k) for k in comb]
+    for j in each_sw_resource:
+        if len(j)==1:
+            continue
+        std_sw_resource.append(statistics.stdev(j))
+    top_combination_lengths = [length for avail, length in combination_lengths[:top_x_count]]
+    #print(top_combination_lengths)
+
+    count_software = [top_combination_lengths.count(i) for i in range(1,num_software+1)]
+
+    # 標準偏差のCDFを計算し、プロット
+    std_sw_resource.sort()
+    std_sw_resource_all.sort()
+    cdf = np.arange(len(std_sw_resource)) / float(len(std_sw_resource) - 1)
+    cdf_all = np.arange(len(std_sw_resource_all)) / float(len(std_sw_resource_all) - 1)
+    
+    ax.plot(std_sw_resource, cdf, marker='o', linestyle='-.',label = h_add, color=cm.jet(i/len(h_adds)))
+    print(len(cdf), len(cdf_all))
+    ax.plot(std_sw_resource_all, cdf_all, marker='x', linestyle = '-',label = "all_{h_add}", color=cm.jet(i/len(h_adds)))
+    
+    i+=1
+
 ax.set_xlabel('Standard Deviation of SW Resource')
 ax.set_ylabel('CDF')
+ax.legend()
 ax.set_title('CDF of Standard Deviation of SW Resource')
 plt.grid(True)
 plt.show()
